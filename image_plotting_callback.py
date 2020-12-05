ec3fb22d5ab7d179014bb643235c827b9044fa17
William Falcon <waf2107@columbia.edu>
2020-12-05 11:00:38 -0400

    vae

---
+++ image_plotting_callback.py
@@ -0,0 +1,41 @@
+from matplotlib.pyplot import imshow, figure
+import numpy as np
+from torchvision.utils import make_grid
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+import pytorch_lightning as pl
+import torch
+
+
+class ImageSampler(pl.Callback):
+    def __init__(self):
+        super().__init__()
+        self.img_size = None
+        self.num_preds = 16
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+        # track the image size
+        if batch_idx == 0:
+            x, _ = batch
+            self.img_size = x.size()
+            self.img_size[0] = self.num_preds
+
+    def on_train_epoch_end(self, trainer, pl_module, outputs):
+        figure(figsize=(8, 3), dpi=300)
+
+        # Z COMES FROM NORMAL(0, 1)
+        rand_v = torch.rand((self.num_preds, pl_module.hparams.latent_dim), device=pl_module.device)
+        p = torch.distributions.Normal(torch.zeros_like(rand_v), torch.zeros_like(rand_v))
+        z = p.rsample()
+
+        # SAMPLE IMAGES
+        with torch.no_grad():
+            pred = pl_module.decoder(z.to(pl_module.device)).cpu()
+
+        # UNDO DATA NORMALIZATION
+        normalize = cifar10_normalization()
+        mean, std = np.array(normalize.mean), np.array(normalize.std)
+        img = make_grid(pred).permute(1, 2, 0).numpy() * std + mean
+
+        # PLOT IMAGES
+        trainer.logger.experiment.add_image('img',torch.tensor(img).permute(2, 0, 1), global_step=trainer.global_step)
+
---
+++ vae.py
@@ -0,0 +1,120 @@
+import pytorch_lightning as pl
+pl.seed_everything(1234)
+from torch import nn
+import torch
+from pl_bolts.models.autoencoders.components import (
+    resnet18_decoder,
+    resnet18_encoder,
+)
+from pl_bolts.datamodules import CIFAR10DataModule, ImagenetDataModule
+from image_plotting_callback import ImageSampler
+from argparse import ArgumentParser
+
+
+class VAE(pl.LightningModule):
+    def __init__(self, enc_out_dim=512, latent_dim=256, input_height=32):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        # encoder, decoder
+        self.encoder = resnet18_encoder(False, False)
+        self.decoder = resnet18_decoder(
+            latent_dim=latent_dim,
+            input_height=input_height,
+            first_conv=False,
+            maxpool1=False
+        )
+
+        # distribution parameters
+        self.fc_mu = nn.Linear(enc_out_dim, latent_dim)
+        self.fc_var = nn.Linear(enc_out_dim, latent_dim)
+
+        # for the gaussian likelihood
+        self.log_scale = nn.Parameter(torch.Tensor([0.0]))
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-4)
+
+    def gaussian_likelihood(self, x_hat, logscale, x):
+        scale = torch.exp(logscale)
+        mean = x_hat
+        dist = torch.distributions.Normal(mean, scale)
+
+        # measure prob of seeing image under p(x|z)
+        log_pxz = dist.log_prob(x)
+        return log_pxz.sum(dim=(1, 2, 3))
+
+    def kl_divergence(self, z, mu, std):
+        # --------------------------
+        # Monte carlo KL divergence
+        # --------------------------
+        # 1. define the first two probabilities (in this case Normal for both)
+        p = torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(std))
+        q = torch.distributions.Normal(mu, std)
+
+        # 2. get the probabilities from the equation
+        log_qzx = q.log_prob(z)
+        log_pz = p.log_prob(z)
+
+        # kl
+        kl = (log_qzx - log_pz)
+        kl = kl.sum(-1)
+        return kl
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+
+        # encode x to get the mu and variance parameters
+        x_encoded = self.encoder(x)
+        mu, log_var = self.fc_mu(x_encoded), self.fc_var(x_encoded)
+
+        # sample z from q
+        std = torch.exp(log_var / 2)
+        q = torch.distributions.Normal(mu, std)
+        z = q.rsample()
+
+        # decoded
+        x_hat = self.decoder(z)
+
+        # reconstruction loss
+        recon_loss = self.gaussian_likelihood(x_hat, self.log_scale, x)
+
+        # kl
+        kl = self.kl_divergence(z, mu, std)
+
+        # elbo
+        elbo = (kl - recon_loss)
+        elbo = elbo.mean()
+
+        self.log_dict({
+            'elbo': elbo,
+            'kl': kl.mean(),
+            'recon_loss': recon_loss.mean(),
+            'reconstruction': recon_loss.mean(),
+            'kl': kl.mean(),
+        })
+
+        return elbo
+
+
+def train():
+    parser = ArgumentParser()
+    parser.add_argument('--gpus', type=int, default=None)
+    parser.add_argument('--dataset', type=str, default='cifar10')
+    args = parser.parse_args()
+
+    if args.dataset == 'cifar10':
+        dataset = CIFAR10DataModule('.')
+    if args.dataset == 'imagenet':
+        dataset = ImagenetDataModule('.')
+
+    sampler = ImageSampler()
+
+    vae = VAE()
+    trainer = pl.Trainer(max_epochs=30, progress_bar_refresh_rate=10, callbacks=[sampler])
+    trainer.fit(vae, dataset)
+
+
+if __name__ == '__main__':
+    train()
